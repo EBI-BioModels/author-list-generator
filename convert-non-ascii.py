25cd6dd29cb03e596dbd324a545db3a667456efa
Tung Nguyen <nvntung@gmail.com>
2024-08-23 19:35:36 +0100

    Test converting non-ascii characters

---
+++ convert-non-ascii.py
@@ -0,0 +1,24 @@
+# Import unidecode module from unidecode
+from unidecode import unidecode
+
+# Get transliteration for following
+# non-ASCII text (Unicode string)
+print(unidecode(u'ko\u017eu\u0161\u010dek'))
+
+# Get transliteration for following
+# non-ASCII text (Devanagari)
+print(unidecode("आप नीचे अपनी भाषा और इनपुट उपकरण चुनें और लिखना आरंभ करें"))
+
+# Get transliteration for following
+# non-ASCII text (Chinese)
+print(unidecode("谢谢你"))
+
+# Get transliteration for following
+# non-ASCII text (Japanese)
+print(unidecode("ありがとう。"))
+
+# Get transliteration for following
+# non-ASCII text (Russian)
+print(unidecode("улыбаться Владимир Путин"))
+
+print(unidecode("Eberhard Karl University of Tübingen, Tübingen 72076"))
